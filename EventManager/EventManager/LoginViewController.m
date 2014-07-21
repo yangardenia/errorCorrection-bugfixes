d90c82eca12386faec7800207999a66393adec79
swweaper5 <503519050@qq.com>
2014-07-22 03:16:39 +0800

    modify login

--- EventManager/EventManager/LoginViewController.m
+++ EventManager/EventManager/LoginViewController.m
@@ -8,7 +8,9 @@
 
 #import "LoginViewController.h"
 
-@interface LoginViewController ()
+NSString *const xiaoxia = @"yangardenia";
+
+@interface LoginViewController ()<UITextFieldDelegate>
 
 @property (nonatomic, strong) UITextField *passwordInput;
 
@@ -31,7 +33,8 @@
     [self.view setBackgroundColor:[UIColor lightGrayColor]];
 
     self.passwordInput = [[UITextField alloc] initWithFrame:CGRectMake(85, 150, 150, 30)];
-    self.passwordInput.keyboardType = UIKeyboardTypePhonePad;
+    self.passwordInput.keyboardType = UIKeyboardTypeDefault;
+    self.passwordInput.delegate = self;
     self.passwordInput.placeholder = @"输入密码";
     [self.passwordInput becomeFirstResponder];
     self.passwordInput.borderStyle = UITextBorderStyleLine;
@@ -44,15 +47,24 @@
     [login setBackgroundColor:[UIColor greenColor]];
     [login addTarget:self action:@selector(click) forControlEvents:UIControlEventTouchDown];
     [self.view addSubview:login];
-    // Do any additional setup after loading the view.
+    
+    [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(textFiledEditChanged:) name:UITextFieldTextDidChangeNotification object:nil];
 }
 
 - (void)click {
-    if ([self.passwordInput.text isEqual:@"yangardenia"]) {
+    if ([self.passwordInput.text isEqual:xiaoxia]) {
         [self.navigationController dismissViewControllerAnimated:YES completion:nil];
     } else {
         UIAlertView *alert = [[UIAlertView alloc] initWithTitle:nil message:@"密码不对" delegate:nil cancelButtonTitle:nil otherButtonTitles:@"ok", nil];
         [alert show];
+    }
+}
+
+-(void)textFiledEditChanged:(NSNotification *)obj{
+    UITextField *textField = (UITextField *)obj.object;
+    NSString *toBeString = textField.text;
+    if ([toBeString isEqualToString:xiaoxia]) {
+        [self click];
     }
 }
 
